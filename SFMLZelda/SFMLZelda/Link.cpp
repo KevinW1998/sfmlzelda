873ab8c3960a1316d37a1108d724e66bd200bb86
KevinW1998 <kevin.waldock@gmail.com>
2014-04-18 17:14:13 +0200

    Connect SpriteSheet to Link.cpp

--- SFMLZelda/SFMLZelda/Link.cpp
+++ SFMLZelda/SFMLZelda/Link.cpp
@@ -5,11 +5,17 @@
 Link::Link() : Entity(false)
 {
 	//Init
+	std::string spriteSheetPath = "images/Zelda_Links_Awakening_SpriteSheet.psd";
+	sf::Image someImg;
+	someImg.loadFromFile(spriteSheetPath);
 }
 
 Link::Link(int x, int y) : Entity(false,x,y)
 {
 	//Init
+	std::string spriteSheetPath = "images/Zelda_Links_Awakening_SpriteSheet.psd";
+	sf::Image someImg;
+	someImg.loadFromFile(spriteSheetPath);
 }
 
 
